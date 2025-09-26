1f36537ef613d13ddcb332b05a9a1721755c2707
Mati Kljukin <matik@matik-5420.ria.ee>
2025-09-26 15:40:11 +0300

    Update example references and defaults

--- app/constants.py
+++ app/constants.py
@@ -34,6 +34,9 @@ try:
 except OSError:
     SERVICE_CHECKLIST = "{}"
 
-DEFAULT_SAMPLE_QUESTION = "Create a service that returns the current national holidays of the current year, for Estonia, from start of the year to the current day. Use this api : https://openholidaysapi.org/swagger/v1/swagger.json"
+# DEFAULT_SAMPLE_QUESTION = "Create a service that returns the current national holidays of the current year, for Estonia, from start of the year to the current day. Use this api : https://openholidaysapi.org/swagger/v1/swagger.json"
+
+DEFAULT_SAMPLE_QUESTION = "Create a service that Provides information for 5 most recent active initiatives. Use this api example : https://rahvaalgatus.ee/initiatives?for=parliament&phase=sign&signedSince=2020-01-01&order=-signatureCount&signingEndsAt=2015-06-18T13%3A37%3A42Z&limit=5"
+
 
 DEFAULT_OUTPUT_PATH = Path(__file__).resolve().parent.parent / "service_response.json"
